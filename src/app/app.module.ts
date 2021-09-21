2cc1a149434bcc8d2f8e331057279ba2f2c0ae93
jcbackenddev <jcbackenddev@gmail.com>
2021-09-21 13:16:06 +0200

    added slides component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { SlidesComponent } from './slides/slides.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SlidesComponent
   ],
   imports: [
     BrowserModule,
